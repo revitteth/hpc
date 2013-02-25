0592fc0b14b3f27884c6a9863b98234ab565cef1
Max Revitt <max.revitt@gmail.com>
2013-02-25 17:03:41 +0000

    starting to task the matmatmul

--- src/mat_mat_mul_tbb.hpp
+++ src/mat_mat_mul_tbb.hpp
@@ -1,50 +1,58 @@
 #ifndef mat_mat_mul_tbb_hpp
 #define mat_mat_mul_tbb_hpp
 
-#include "tbb/parallel_for.h"
 #include "mat_t.hpp"
+#include "tbb/task.h"
+#include "tbb/task_scheduler_init.h"
 
 using namespace tbb;
 
-void mat_mat_mul(mat_t dst,	const mat_t a, const mat_t b)
+class MatMatMulWorker : public tbb::task 
 {
-	if((dst.rows==1) || (dst.cols==1))
-	{
-		for(unsigned row=0;row<dst.rows;row++)
+	public:
+		MatMatMulWorker(mat_t dst, const mat_t a, const mat_t b) : dst(dst), a(a), b(b) {}
+
+		task* execute(void)
 		{
-			for(unsigned col=0;col<dst.cols;col++)
+			if((dst.rows==1) || (dst.cols==1))
 			{
-				double acc=0.0;
-				for(unsigned i=0;i<a.cols;i++)
+				for(unsigned row=0;row<dst.rows;row++)
 				{
-					acc += a.at(row,i) * b.at(i,col);
+					for(unsigned col=0;col<dst.cols;col++)
+					{
+						double acc=0.0;
+						for(unsigned i=0;i<a.cols;i++)
+						{
+							acc += a.at(row,i) * b.at(i,col);
+						}
+						dst.at(row,col) = acc;
+					}
 				}
-				dst.at(row,col) = acc;
 			}
-		}
-	}
-	else
-	{
-		local_mat_t right(dst.rows, dst.cols);
-		
-		mat_mat_mul(dst.quad(0,0), a.quad(0,0), b.quad(0,0));
-		mat_mat_mul(dst.quad(0,1), a.quad(0,0), b.quad(0,1));
-		mat_mat_mul(dst.quad(1,0), a.quad(1,0), b.quad(0,0));
-		mat_mat_mul(dst.quad(1,1), a.quad(1,0), b.quad(0,1));
-		
-		mat_mat_mul(right.quad(0,0), a.quad(0,1), b.quad(1,0));
-		mat_mat_mul(right.quad(0,1), a.quad(0,1), b.quad(1,1));
-		mat_mat_mul(right.quad(1,0), a.quad(1,1), b.quad(1,0));
-		mat_mat_mul(right.quad(1,1), a.quad(1,1), b.quad(1,1));
-		
-		for(unsigned row=0;row<dst.rows;row++)
-		{
-			for(unsigned col=0;col<dst.cols;col++)
+			else
 			{
-				dst.at(row,col) += right.at(row,col);
+				task_list tasks;
+
+				local_mat_t right(dst.rows, dst.cols);
+		
+				tasks.push_back(*new (task::allocate_child()) MatMatMulWorker(dst.quad(0,0), a.quad(0,0), b.quad(0,0));
+				tasks.push_back(*new (task::allocate_child()) MatMatMulWorker(dst.quad(0,1), a.quad(0,0), b.quad(0,1));
+				tasks.push_back(*new (task::allocate_child()) MatMatMulWorker(dst.quad(1,0), a.quad(1,0), b.quad(0,0));
+				tasks.push_back(*new (task::allocate_child()) MatMatMulWorker(dst.quad(1,1), a.quad(1,0), b.quad(0,1));
+		
+				tasks.push_back(*new (task::allocate_child()) MatMatMulWorker(right.quad(0,0), a.quad(0,1), b.quad(1,0));
+				tasks.push_back(*new (task::allocate_child()) MatMatMulWorker(right.quad(0,1), a.quad(0,1), b.quad(1,1));
+				tasks.push_back(*new (task::allocate_child()) MatMatMulWorker(right.quad(1,0), a.quad(1,1), b.quad(1,0));
+				tasks.push_back(*new (task::allocate_child()) MatMatMulWorker(right.quad(1,1), a.quad(1,1), b.quad(1,1));
+		
+				for(unsigned row=0;row<dst.rows;row++)
+				{
+					for(unsigned col=0;col<dst.cols;col++)
+					{
+						dst.at(row,col) += right.at(row,col);
+					}
+				}
 			}
-		}
-	}
 }
 
 #endif
